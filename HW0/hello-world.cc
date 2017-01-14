e7acaa9c26cf8cc113552226f830c17eb6a292df
Linh Thi Xuan Phan <linhphan@cis.upenn.edu>
2017-01-14 14:09:03 -0500

    Added HW0

---
+++ HW0/hello-world.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  // TODO: Change the text below to 'Hello World - this is <yourNAME> (<yourSEASlogin)'
+  // Example: 'Hello World - this is Linh Thi Xuan Phan (linhphan)'
+
+  printf("Your text goes here\n");
+  
+  return 0;
+}
