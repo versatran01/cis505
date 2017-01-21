234c1adba224350361711e728cce707792bca3a5
Chao Qu <quchao@seas.upenn.edu>
2017-01-21 15:49:56 -0500

    add gitignore and reformat HW1/makeinput.cc

--- HW1/makeinput.cc
+++ HW1/makeinput.cc
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   if (argc != 2) {
-  	fprintf(stderr, "Syntax: %s howManyNumbers\n", argv[0]);
-  	exit(1);
+    fprintf(stderr, "Syntax: %s howManyNumbers\n", argv[0]);
+    exit(1);
   }
-  
+
   srand(time(0));
-  for (int i=0; i<atoi(argv[1]); i++) {
-  	long long v = rand();
-  	for (int j=0; j<3; j++)
-  	  v = (v<<16)+rand();
+  for (int i = 0; i < atoi(argv[1]); i++) {
+    long long v = rand();
+    for (int j = 0; j < 3; j++)
+      v = (v << 16) + rand();
     printf("%lld\n", v);
   }
 
