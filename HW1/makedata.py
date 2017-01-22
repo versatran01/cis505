eb34e92a09cdc1dcaa406cde8e10127f40e5e61d
Chao Qu <quchao@seas.upenn.edu>
2017-01-21 22:37:17 -0500

    Fix include in mysort.cc and add run option in makedata.py

--- HW1/makedata.py
+++ HW1/makedata.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
+from glob import glob
 import argparse as ap
 from os import system
 
+
 parser = ap.ArgumentParser()
-parser.add_argument('num_files', nargs='?', default='4',
+parser.add_argument('--num_files', '-nf', nargs='?', default='4',
                     help='number of files')
-parser.add_argument('--num_integers', nargs='?', default='1000',
+parser.add_argument('--num_integers', '-n1', nargs='?', default='1000',
                     help='number of integers')
+parser.add_argument('--run', '-r', action='store_true', help="run mysort")
+parser.add_argument('--num_processes', '-np', nargs='?', default='4',
+                    help='number of processes')
 args = parser.parse_args()
+
+if args.run:
+    print("Run mysort on input files")
+    files = glob("hw1_input*.txt")
+    files_with_space = " ".join(files)
+    command = "./mysort -n {} {}".format(args.num_processes, files_with_space)
+    print(command)
+    system(command)
+    exit()
 
 num_files = int(args.num_files)
 num_integers = int(args.num_integers)
@@ -16,9 +30,12 @@ print("number of files: ", num_files)
 print("number of integers: ", num_integers)
 
 # run make first so that we have the binary makeinput
-system('make')
+system('make clean && make')
 
+# Generate files
 for i_file in range(num_files):
     command = "./makeinput {} > hw1_input{}.txt".format(num_integers, i_file)
     print(command)
     system(command)
+
+
--- HW1/mysort.cc
+++ HW1/mysort.cc
@@ -1,14 +1,24 @@
 #include <argp.h>
 
-#include <string>
-#include <utility>
+#include <cstdlib>
 #include <vector>
+
+#ifdef DEBUG
+#define DEBUG_PRINT(...)          \
+  do {                            \
+    fprintf(stderr, __VA_ARGS__); \
+  } while (false);
+#else
+#define DEBUG_PRINT(...) \
+  do {                   \
+  } while (false)
+#endif
 
 static char args_doc[] = "FILE [FILES...]";
 
 struct arguments {
   int num_processes = 4;
-  char *file; // need at least 1 file
+  char *file;  // need at least 1 file
   char **files;
 };
 
@@ -16,32 +26,44 @@ struct arguments {
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   struct arguments *arguments = (struct arguments *)state->input;
   switch (key) {
-  case 'n':
-    arguments->num_processes = atoi(arg);
-    break;
-  case ARGP_KEY_NO_ARGS:
-    argp_usage(state);
-  case ARGP_KEY_ARG:
-    arguments->file = arg;
-    arguments->files = &state->argv[state->next];
-    state->next = state->argc;
-    break;
-  default:
-    return ARGP_ERR_UNKNOWN;
+    case 'n':
+      arguments->num_processes = std::atoi(arg);
+      break;
+    case ARGP_KEY_NO_ARGS:
+      argp_usage(state);
+    case ARGP_KEY_ARG:
+      arguments->file = arg;
+      arguments->files = &state->argv[state->next];
+      state->next = state->argc;
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
   }
   return 0;
 }
 
 int main(int argc, char *argv[]) {
+  // Parse command line arguments
   struct arguments arguments;
   struct argp_option options[] = {
       {0, 'n', "NUM_PROCESSES", 0, "Number of processes."}, {0}};
   struct argp argp = {options, parse_opt, args_doc, 0};
   int status = argp_parse(&argp, argc, argv, 0, 0, &arguments);
-  printf("Number of processes: %d\n", arguments.num_processes);
-  printf("Files: %s", arguments.file);
-  for (int i = 0; arguments.files[i]; ++i) {
-    printf(", %s", arguments.files[i]);
+
+  if (status) {
+    fprintf(stderr, "Failed to parse arguments, error code: %d", status);
+    exit(status);
   }
-  return status;
+
+  DEBUG_PRINT("Number of processes: %d\n", arguments.num_processes);
+  DEBUG_PRINT("Input files: %s", arguments.file);
+  for (int i = 0; arguments.files[i]; ++i) {
+    DEBUG_PRINT(", %s", arguments.files[i]);
+  }
+  DEBUG_PRINT("\n")
+
+
+  // Read data from files
+
+  return 0;
 }
