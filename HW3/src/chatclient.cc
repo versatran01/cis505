e9fdc07ef7a10979e3c29a741488984fae9df6f4
Chao Qu <quchao@seas.upenn.edu>
2017-03-18 16:54:54 -0400

    start client

--- HW3/src/chatclient.cc
+++ HW3/src/chatclient.cc
@@ -1,13 +1,34 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+
+#include <string>
+
+#include "cmdparser.hpp"
+#define LOGURU_IMPLEMENTATION 1
+#include "loguru.hpp"
+
+void ConfigureParser(cli::Parser &parser) {
+  parser.set_required<std::string>("", "server address and port",
+                                   "IP address and port number of server");
+  parser.set_optional<bool>("l", "log to file", false,
+                            "Log to file if set, otherwise log to stderr");
+}
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  if (argc == 1) {
     fprintf(stderr, "*** Author: Chao Qu (quchao)\n");
     exit(1);
   }
 
-  /* Your code here */
+  cli::Parser parser(argc, argv);
+  ConfigureParser(parser);
+  parser.run_and_exit_if_error();
+
+  // Setup log
+  if (parser.get<bool>("l")) {
+    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
+    loguru::add_file("chatclient.log", loguru::Truncate, loguru::Verbosity_MAX);
+  }
 
   return 0;
-}  
+}
--- HW3/src/chatserver.cc
+++ HW3/src/chatserver.cc
@@ -1,13 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+
+#define LOGURU_IMPLEMENTATION 1
+#include "loguru.hpp"
+
+#include "cmdparser.hpp"
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
+  if (argc == 1) {
     fprintf(stderr, "*** Author: Chao Qu (quchao)\n");
     exit(1);
   }
 
-  /* Your code here */
-
   return 0;
-}  
+}
