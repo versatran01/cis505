7564919fb67d67ca93e094db34c3bbcf5441d961
Chao Qu <quchao@seas.upenn.edu>
2017-03-19 15:46:13 -0400

    update

--- HW3/src/chatclient.cc
+++ HW3/src/chatclient.cc
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <mutex>
+#include <regex>
 #include <string>
 
 #include "cmdparser.hpp"
@@ -35,25 +36,40 @@ int main(int argc, char *argv[]) {
     loguru::add_file("chatclient.log", loguru::Truncate, loguru::Verbosity_MAX);
   }
 
+  // Parse server address and port
+  const auto addr_port = parser.get<std::string>("");
+  LOG_F(INFO, "argv: %s", addr_port.c_str());
+  std::string pattern =
+      "^([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]+)\\:([0-9]{1,5})$";
+  std::regex addr_port_regex(pattern);
+  std::smatch results;
+  if (!std::regex_search(addr_port, results, addr_port_regex))
+    LOG_F(FATAL, "Invalid address and port.");
+
+  const auto addr = results.str(1);
+  const auto port = std::atoi(results.str(2).c_str());
+  LOG_F(INFO, "addr: %s, port: %d", addr.c_str(), port);
+
   // Setup connection
-  int sock = socket(AF_INET, SOCK_DGRAM, 0);
-  struct sockaddr_in dest;
-  bzero(&dest, sizeof(dest));
-  dest.sin_family = AF_INET;
-  dest.sin_port = htons(4711);
-  inet_pton(AF_INET, "127.0.0.1", &(dest.sin_addr));
-  sendto(sock, "abc", strlen("abc"), 0, (struct sockaddr *)&dest, sizeof(dest));
+  int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  inet_pton(AF_INET, addr.c_str(), &(server_addr.sin_addr));
+  sendto(sock_fd, "abc", strlen("abc"), 0, (struct sockaddr *)&server_addr,
+         sizeof(server_addr));
 
   char buf[100];
   struct sockaddr_in src;
-  socklen_t srcSize = sizeof(src);
-  int rlen =
-      recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&src, &srcSize);
+  socklen_t src_size = sizeof(src);
+  int rlen = recvfrom(sock_fd, buf, sizeof(buf), 0, (struct sockaddr *)&src,
+                      &src_size);
   buf[rlen] = 0;
   printf("Echo: [%s] (%d byptes) from %s\n", buf, rlen,
          inet_ntoa(src.sin_addr));
 
-  close(sock);
+  close(sock_fd);
 
   return 0;
 }
--- HW3/src/udpechoserver.cc
+++ HW3/src/udpechoserver.cc
@@ -1,31 +1,41 @@
 #include <arpa/inet.h>
+#include <cstdio>
+#include <cstring>
+#include <string>
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
+/**
+ * @brief Simple udp echoserver for testing
+ */
+int main(int argc, char **argv) {
+  int port = 0;
+  if (argc > 1) {
+    port = std::atoi(argv[1]);
+  }
 
-int main() {
+  if (port < 1024) {
+    fprintf(stderr, "port %d < 1024, use default 8000.\n", port);
+    port = 8000;
+  }
+
   int sock = socket(AF_INET, SOCK_DGRAM, 0);
-  
+
   struct sockaddr_in serv_addr;
   bzero(&serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
-  serv_addr.sin_port = htons(4711);
-  bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-  
+  serv_addr.sin_port = htons(port);
+  bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
   while (true) {
     struct sockaddr_in src;
     socklen_t srclen = sizeof(src);
-    char buf[100];
-    int rlen = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr*)&src,
+    char buf[1024];
+    int rlen = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&src,
                         &srclen);
     buf[rlen] = 0;
-    
+
     printf("Echoing [%s] to %s\n", buf, inet_ntoa(src.sin_addr));
-    sendto(sock, buf, rlen, 0, (struct sockaddr*)&src, sizeof(src));
+    sendto(sock, buf, rlen, 0, (struct sockaddr *)&src, sizeof(src));
   }
-
 }
-
-
